b0a7b45020b2b6071cb4ea0724c888b9a5fe5cd3
Georg Veichtlbauer <georg@vware.at>
2024-07-20 11:51:09 +0700

    CAP: Add IMS and EGL symlinks
    Change-Id: I536ef36ce3725be3fd34e0a10a3d703b5afd4985

--- CAP-vendor.mk
+++ CAP-vendor.mk
@@ -1348,3 +1348,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsconfig-V2.1-java \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
